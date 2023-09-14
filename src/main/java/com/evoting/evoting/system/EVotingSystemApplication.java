5dd5b00cbca491d1c837da7278266426060708d8
kenny <kehinderaji29@gmail.com>
2023-09-14 17:17:26 +0100

    refactored

--- src/main/java/com/evoting/evoting/system/EVotingSystemApplication.java
+++ src/main/java/com/evoting/evoting/system/EVotingSystemApplication.java
@@ -3,10 +3,26 @@ package com.evoting.evoting.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EVotingSystemApplication {
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
 
+				registry
+						.addMapping("/**")
+						.allowedOrigins("*", "http://127.0.0.1", "http://example.org", "http://localhost", "http://localhost:5500")
+						.allowedHeaders("*")
+						.allowedMethods("POST", "GET", "PUT", "PATCH", "OPTIONS");
+			}
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EVotingSystemApplication.class, args);
 	}
